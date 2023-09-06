32ebca6324b1cade504b5de1accf3ccde218c69f
James Ignacio Olaya Garzon <jamesmixolaya@gmail.com>
2023-09-06 11:24:24 -0500

    version0.1

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('create_occount', views.create_account, name='occount'),
+    path('recover_password', views.recovery_password, name='recover')
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,2 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+def index(request):
+    pass
+
+def login(request):
+    pass
+
+def create_account(request):
+    pass
+def recovery_password(request):
+    pass
