2e258f7b8edcc5807a5472a2ebf66d550a265038
nicolle valeria tique astudillo <jamesmixolaya@gmail.com>
2023-09-25 05:42:45 -0500

    james version 1.14(esqueletos)

--- api/models.py
+++ api/models.py
@@ -16,24 +16,30 @@ class Cliente(models.Model):
     usuario = models.CharField(max_length=20, null=False, blank=False)
     password = models.CharField(max_length=20, null=False, blank=False)
 
+class Material(models.Model):
+    nombreM=models.CharField(max_length=20, null=False, blank=False)
+    precioM=models.BigIntegerField( null=False, blank=False)
+
 class Inventario(models.Model):
     producto= models.CharField(max_length=10, null=False, blank=False)
     cantidad= models.IntegerField(null=False, blank=False)
     reutilizable= models.CharField(max_length=10, null=False, blank=False)
     bodega= models.CharField(max_length=10, null=False, blank=False)
-
-
+    
+class Producto(models.Model):
+    nombreP=models.CharField(max_length=30,null=False, blank=False)
+    predioP=models.BigIntegerField(null=False, blank=False)
 
 class Pedido(models.Model):
     descripccion = models.CharField(max_length=200, null=False, blank=False)
     Proyecto = models.CharField(max_length=20, null=False, blank=False)
-    idCliente = models.IntegerField( null=False, blank=False)
+    idCliente = models.ForeignKey(Cliente,on_delete=models.CASCADE )
     cotizacion = models.CharField(max_length=10, null=False, blank=False)
     direccion = models.CharField(max_length=10, null=False, blank=False)
 
-
-
 class Estado(models.Model):
-    idProducto = models.IntegerField()
-    idCliente = models.IntegerField()
+    idProducto = models.ForeignKey(Producto,on_delete=models.CASCADE )
+    idCliente = models.ForeignKey(Cliente,on_delete=models.CASCADE )
     direccion = models.CharField(max_length=10)
+
+
--- api/views.py
+++ api/views.py
@@ -3,9 +3,9 @@ from django.http.response import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from .forms import create_cliente, create_user, create_encargado,crear_pedido
+from .forms import  create_user,create_producto ,crear_pedido
 import json
-from .models import Usuario, Encargado, Cliente, Pedido
+from .models import Usuario, Encargado, Cliente, Pedido, Producto
 
 
 
@@ -32,29 +32,21 @@ def login(request):
         else:
             return redirect('occount')
 
-            
-
-
-def create_account(requst):
-    if requst.method == 'GET':
-        return render(requst, 'create_account/create_account.html',{
+def create_account(request):
+    if request.method == 'GET':
+        return render(request, 'create_account/create_account.html',{
             'form': create_user,
         })
     else:
-        nombre = requst.POST['nombre']
-        usuario = requst.POST['usuario']
-        correo = requst.POST['correo']
-        telefono = requst.POST['telefono']
-        password = requst.POST['contraseña']
+        nombre = request.POST['nombre']
+        usuario = request.POST['usuario']
+        correo = request.POST['correo']
+        telefono = request.POST['telefono']
+        password = request.POST['contraseña']
 
         Cliente.objects.create(usuario=usuario, password=password)
         Usuario.objects.create(nombre=nombre,usuario=usuario,correo=correo,telefono=telefono,password=password)
         return redirect('/login')
-
-
-
- 
-
     
 def administrador(request):
     return render(request, 'administrador/administrador.html')
@@ -65,11 +57,41 @@ def cliente(request):
 def usuario(request):
     return render(request, 'usuario/usuario.html')
 
+def cambiar_precios(request):
+    if request.method == 'PUT':
+        id=request.PUT['id']
+        productosP = list(Producto.objects.filter(id=id).values())
+        if len(productosP) > 0:
+            producto = Producto.objects.get(id=id)
+            producto.predioP = request.PUT['precio']
+            producto.save()
 
+        
+    else:
+        return render(request, 'create_account/create_account.html',{
+            'form': create_user,
+        })
+    
+def create_product(request):
+    if request.method == 'GET':
+        return render(request, 'create_account/create_account.html',{
+            'form': create_producto,
+        })
+    else:
+        nombre = request.POST['nombre']
+        precio = request.POST['precio']
+        
 
+        Producto.objects.create(nombreP=nombre, precioP=precio)
+        return redirect('/login')
 
-
-
-
-
-
+def delete_material(request):
+    if request.method == 'PUT':
+        id=request.PUT['id']
+        materiales = list(Producto.objects.filter(id=id).values())
+        if len(materiales) > 0:
+            Producto.objects.filter(id=id).delete()
+    else:
+        return render(request, 'create_account/create_account.html',{
+            'form': create_user,
+        })
