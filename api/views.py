9b707f9a437b6b8e0e4fce8dbf421455c9b8240a
nicolle valeria tique astudillo <jamesmixolaya@gmail.com>
2023-12-04 11:28:37 -0500

    version befores revision

--- api/views.py
+++ api/views.py
@@ -36,18 +36,18 @@ def login(request):
                 return redirect('login')
     return redirect('login')
            
-def create_account(requst):
-    if requst.method == 'GET':
-        return render(requst, 'create_account/create_account.html',{
+def create_account(request):
+    if request.method == 'GET':
+        return render(request, 'create_account/create_account.html',{
             'form': create_user,
             
         })
     else:
-        nombre = requst.POST['nombre']
-        usuario = requst.POST['usuario']
-        correo = requst.POST['correo']
-        telefono = requst.POST['telefono']
-        password = requst.POST['contraseña']
+        nombre = request.POST['nombre']
+        usuario = request.POST['usuario']
+        correo = request.POST['correo']
+        telefono = request.POST['telefono']
+        password = request.POST['contraseña']
         if nombre != '' and usuario != '' and correo != '' and telefono != '' and password != '':
             Usuario.objects.create(nombre=nombre,usuario=usuario,correo=correo,telefono=telefono,password=password)
             
@@ -87,7 +87,7 @@ def administrador(request):
 
 
 def cliente(request):
-    dato = 'El valor de su cotizacion es de'
+    dato = 'El valor de la cotizacion de una:'
     if request.method == 'GET':
         return render(request, 'cliente/cliente.html')  
     else:
@@ -101,32 +101,40 @@ def cliente(request):
         if (select == 'Puertas' ):
             material = request.POST['materialP']
             tipo = request.POST['tipoP']
+            marco = request.POST['marcoP']
+            manija = request.POST['manijaP']
             altura = float(request.POST['alturaP'])
             ancho = float(request.POST['anchoP'])
 
             if (material == 'vidrioS'):
                 for data in materiales:
                     if data.nombreM == 'vidrios con control solar':
+                        material = 'vidrios con control solar'
                         valor = data.precioM
             elif (material == 'vidrioE'):
                 for data in materiales:
                     if data.nombreM == 'vidrios bajo emisivos':
+                        material = 'vidrios bajo emisivos'
                         valor = data.precioM
             elif (material == 'vidrioT'):
                 for data in materiales:
                     if data.nombreM == 'vidrio templado':
+                        material = 'vidrio templado'
                         valor = data.precioM            
             elif (material == 'vidrioD'):
                 for data in materiales:
                     if data.nombreM == 'vidrios con doble acristalamiento':
+                        material = 'vidrios con doble acristalamiento'
                         valor = data.precioM
             elif (material == 'vidrioL'):
                 for data in materiales:
                     if data.nombreM == 'Vidrio laminado':
+                        material = 'Vidrio laminado'
                         valor = data.precioM
             elif (material == 'vidrioF'):
                 for data in materiales:
                     if data.nombreM == 'vidrio flotado':
+                        material = 'vidrio flotado'
                         valor = data.precioM                
             if (tipo == 'sencilla'):
                 valor2 = 1
@@ -141,7 +149,8 @@ def cliente(request):
             print(cotizacion)
             return render(request, 'cliente/cliente.html',{
             'datos': dato,
-            'valor': round(cotizacion)
+            'valor': round(cotizacion),
+            'pedido': select + ' de ' + material + ' con marco de '+ marco + ' con manijas ' + manija + ' es de:'
         })            
         elif(select == 'Ventanas'):
             material = request.POST['materialV']
@@ -411,105 +420,3 @@ def cliente(request):
 def usuario(request):
     return render(request, 'usuario/usuario.html')
 
-def cambiar_precios(request):
-    if request.method == 'PUT':
-        id=request.PUT['id']
-        productosP = list(Producto.objects.filter(id=id).values())
-        if len(productosP) > 0:
-            producto = Producto.objects.get(id=id)
-            producto.predioP = request.PUT['precio']
-            producto.save()
-
-        
-    else:
-        return render(request, 'create_account/create_account.html',{
-            'form': create_user,
-        })  
-    
-def create_product(request):
-    if request.method == 'GET':
-        return render(request, 'administrador/administrador.html',{
-            'form': create_material,
-        })
-    else:
-        nombre = request.POST['material-name']
-        precio = request.POST['material-price']
-        cantidad = request.POST['material-cantidad']
-        
-
-        Material.objects.create(nombreM=nombre, precioM=precio , cantidadM = cantidad)
-        return redirect('/admins')
-
-def delete_material(request):
-    if request.method == 'delete':
-        id=request.PUT['id']
-        materiales = list(Producto.objects.filter(id=id).values())
-        if len(materiales) > 0:
-            Producto.objects.filter(id=id).delete()
-    else:
-        return render(request, 'create_account/create_account.html',{
-            'form': create_user,
-        })
-    
-#class ProductView(View):
-
-
-
-
-#    @method_decorator(csrf_exempt)
-#    def dispatch(self, request, *args, **kwargs):
-#        return super().dispatch(request, *args, **kwargs)
-    
-#    def get(self, request,id=0):
-        if (id>0):
-            products=list(Producto.objects.filter(id=id).values())
-            if len(products )>0:
-                product=products[0]
-                datos = {'message': "Success", 'product': product}
-            else:
-                datos = {'message': "products not found..."}
-            return (datos)
-        else:
-            products=list(Producto.objects.values())
-            if len(products )> 0:
-                datos = {'message': "Success", 'products': products}
-            else:
-                datos = {'message': "products not found..."}
-                
-            return (datos)
-        
-
-#    def post(self, request):
-        # print(request.body)
-#        jd =json.loads(request.body)
-        # print(jd)
-#        Producto.objects.create(name=jd['name'], descripccion=jd['descripccion'], precio=jd['precio'], stock=jd['stock'])
-#        datos = {'message': "Success"}
-#        return (datos)
-
- #   def put(self, request, id):
-        jd =json.loads(request.body)
-        products=list(Producto.objects.filter(id=id).values())
-        if len(products )>0:
-            product = Producto.objects.get(id=id)
-            product.name =jd['name']
-            product.descripccion = jd['descripccion']
-            product.precio =jd['precio']
-            product.stock =jd['stock']
-            product.save()
-            datos = {'message': "Success"}
-        else:
-            datos = {'message': "product not found..."}
-        return (datos)
-    
-#    def delete(self, request,id):
-        products = list(Producto.objects.filter(id=id).values())
-        if len(products) > 0:
-            Producto.objects.filter(id=id).delete()
-            datos = {'message': "Success"}
-
-        else:
-            datos = {'message': "product not found..."}
-        return (datos)
-
-        
